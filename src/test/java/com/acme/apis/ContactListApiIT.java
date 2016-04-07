b3b3c7f6758d204e932a02f25576719a54dc2cba
yotamm <yotamm@il.ibm.com>
2016-04-07 14:27:33 +0300

    commit it!

--- src/test/java/com/acme/apis/ContactListApiIT.java
+++ src/test/java/com/acme/apis/ContactListApiIT.java
@@ -11,6 +11,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = IntegrationTestConfig.class)
@@ -38,62 +40,125 @@ public class ContactListApiIT {
         contact.name = "tedy";
         contact.phoneNumber = "12345";
         //Test add new contact
-        ResponseEntity operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, "");
+        ResponseEntity operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, DEFAULT_SCOPE);
         Assert.assertEquals(201, operationResult.getStatusCode().value());
         String location = operationResult.getHeaders().get("Location").toString();
         Assert.assertTrue(location.contains("/tedy"));
 
         //Make sure the contact is there
-        ResponseEntity<Contact> contactResp = restUtil.performRequest("/tedy", Contact.class, null, HttpMethod.GET, "");
+        ResponseEntity<Contact> contactResp = restUtil.performRequest("/tedy", Contact.class, null, HttpMethod.GET, DEFAULT_SCOPE);
         Assert.assertEquals(200, contactResp.getStatusCode().value());
         Assert.assertEquals("tedy", contactResp.getBody().name);
         Assert.assertEquals("12345", contactResp.getBody().phoneNumber);
 
         //Try to add the same contact twice (expect conflict)
-        operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, "");
+        operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, DEFAULT_SCOPE);
         Assert.assertEquals(409, operationResult.getStatusCode().value());
 
         //Just clean up
-        ResponseEntity deleteResp = restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, "");
+        ResponseEntity deleteResp = restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
         Assert.assertEquals(200, deleteResp.getStatusCode().value());
 
         //Make sure contact is not there after delete
-        contactResp = restUtil.performRequest("/tedy", Contact.class, null, HttpMethod.GET, "");
+        contactResp = restUtil.performRequest("/tedy", Contact.class, null, HttpMethod.GET, DEFAULT_SCOPE);
         Assert.assertEquals(404, contactResp.getStatusCode().value());
     }
 
-
-    /**
-     * - Add contact
-     * - Delete the contact we just added (expected HTTP status: 200)
-     * - Make sure the contact is not there after deletion (status: 404)
-     * - Try to delete the same contact one more time and expect 404
-     */
     @Test
-    public void testDeleteContact(){
-        //TODO implement
+    public void testDeleteContact() throws IOException {
+        //delete the contact (and ignore the result) just in case the DB was dirty
+        restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+
+        Contact contact = new Contact();
+        contact.name = "tedy";
+        contact.phoneNumber = "12345";
+        //Test add new contact
+        ResponseEntity operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, DEFAULT_SCOPE);
+        Assert.assertEquals(201, operationResult.getStatusCode().value());
+
+        //Delete the contact
+        ResponseEntity deleteResp = restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+        Assert.assertEquals(200, deleteResp.getStatusCode().value());
+
+        //Make sure the contact is not there
+        ResponseEntity<Contact> contactResp = restUtil.performRequest("/tedy", Contact.class, null, HttpMethod.GET, DEFAULT_SCOPE);
+        Assert.assertEquals(404, contactResp.getStatusCode().value());
+
+        //Make sure cannot delete twice
+        deleteResp = restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+        Assert.assertEquals(404, deleteResp.getStatusCode().value());
     }
 
 
-    /**
-     * - Add contact
-     * - Then update the contact's phone number using PUT, (expect status 200)
-     * - Get the contact and make sure the phone number was updated
-     * - Delete the contact
-     * - Try to update again and make sure we got 404
-     */
     @Test
-    public void testUpdateContact() {
-        //TODO implement
+    public void testUpdateContact() throws IOException {
+        //delete the contact (and ignore the result) just in case the DB was dirty
+        restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+
+        Contact contact = new Contact();
+        contact.name = "tedy";
+        contact.phoneNumber = "12345";
+        //Test add new contact
+        ResponseEntity operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, DEFAULT_SCOPE);
+        Assert.assertEquals(201, operationResult.getStatusCode().value());
+
+        contact.phoneNumber = "111";
+
+        operationResult = restUtil.performRequest("/", null, contact, HttpMethod.PUT, DEFAULT_SCOPE);
+        Assert.assertEquals(200, operationResult.getStatusCode().value());
+
+        //Make sure the contact is updated
+        ResponseEntity<Contact> contactResp = restUtil.performRequest("/tedy", Contact.class, null, HttpMethod.GET, DEFAULT_SCOPE);
+        Assert.assertEquals(200, contactResp.getStatusCode().value());
+        Assert.assertEquals("111", contactResp.getBody().phoneNumber); //assert we got the updated phone number
+
+        //Delete the contact
+        restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+
+        //Make sure it is not possible to update after deletion
+        operationResult = restUtil.performRequest("/", null, contact, HttpMethod.PUT, DEFAULT_SCOPE);
+        Assert.assertEquals(404, operationResult.getStatusCode().value());
     }
 
-    /**
-     * - Add 2 contacts
-     * - Get all contacts and make sure the 2 contacts we've added are part of the list
-     */
     @Test
-    public void testGetAllContacts(){
-        //TODO implement
+    public void testGetAllContacts() throws IOException {
+        //delete the contact (and ignore the result) just in case the DB was dirty
+        restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+        restUtil.performRequest("/tedy2", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+
+        Contact contact = new Contact();
+        contact.name = "tedy";
+        contact.phoneNumber = "12345";
+        //Test add new contact
+        ResponseEntity operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, DEFAULT_SCOPE);
+        Assert.assertEquals(201, operationResult.getStatusCode().value());
+
+        contact.name = "tedy2";
+        contact.phoneNumber = "1212";
+        operationResult = restUtil.performRequest("/", null, contact, HttpMethod.POST, DEFAULT_SCOPE);
+        Assert.assertEquals(201, operationResult.getStatusCode().value());
+
+        ResponseEntity<Contact[]> allContacts = restUtil.performRequest("/", Contact[].class, null, HttpMethod.GET, DEFAULT_SCOPE);
+        Assert.assertEquals(200, allContacts.getStatusCode().value());
+
+        Map<String, Contact> contactMap = new HashMap<String, Contact>();
+        for (Contact con : allContacts.getBody()){
+            contactMap.put(con.name, con);
+        }
+
+        Contact tedy = contactMap.get("tedy");
+        Contact tedy2 = contactMap.get("tedy2");
+
+        Assert.assertNotNull(tedy);
+        Assert.assertNotNull(tedy2);
+
+        Assert.assertEquals("12345", tedy.phoneNumber);
+        Assert.assertEquals("1212", tedy2.phoneNumber);
+
+        //Cleanup
+        restUtil.performRequest("/tedy", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
+        restUtil.performRequest("/tedy2", null, null, HttpMethod.DELETE, DEFAULT_SCOPE);
     }
+
 
 }
