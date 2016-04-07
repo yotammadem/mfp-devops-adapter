12de982f5cdc70f6cad8e257531813810201bd55
yotamm <yotamm@il.ibm.com>
2016-04-07 17:40:52 +0300

    add parameter

--- src/main/java/com/acme/apis/ContactListApiApplication.java
+++ src/main/java/com/acme/apis/ContactListApiApplication.java
@@ -10,14 +10,33 @@ package com.acme.apis;
 import java.util.logging.Logger;
 import java.util.*;
 
+import com.acme.apis.models.Contact;
+import com.ibm.mfp.adapter.api.ConfigurationAPI;
 import com.ibm.mfp.adapter.api.MFPJAXRSApplication;
+
+import javax.ws.rs.core.Context;
 
 public class ContactListApiApplication extends MFPJAXRSApplication{
 
 	static Logger logger = Logger.getLogger(ContactListApiApplication.class.getName());
 	
 
+	@Context
+	ConfigurationAPI configurationAPI;
+
 	protected void init() throws Exception {
+		String db = configurationAPI.getPropertyValue("db");
+		if (db.equalsIgnoreCase("db1")){
+			Map<String, Contact> contacts = ContactListApiResource.contactMap;
+			contacts.clear();
+			contacts.put("Sagy", new Contact("Sagy","1234"));
+			contacts.put("Yotam", new Contact("Yotam","1234"));
+		}else{
+			Map<String, Contact> contacts = ContactListApiResource.contactMap;
+			contacts.clear();
+			contacts.put("James", new Contact("James","5555"));
+			contacts.put("Mac", new Contact("Mac","6666"));
+		}
 		logger.info("Adapter initialized!");
 	}
 	
--- src/main/java/com/acme/apis/models/Contact.java
+++ src/main/java/com/acme/apis/models/Contact.java
@@ -6,4 +6,13 @@ package com.acme.apis.models;
 public class Contact {
     public String name;
     public String phoneNumber;
+
+    public Contact(){
+
+    }
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
